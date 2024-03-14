20249829921b0774729b41de51d862e67bb0eff6
Kira <kira@glorylab.xyz>
2024-03-14 21:49:00 +0900

    Install Tailwind CSS

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import stylesheet from "~/tailwind.css";
 import type { LinksFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
@@ -10,6 +11,7 @@ import {
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
