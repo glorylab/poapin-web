8bd56dcba3e5e1db5590fc0d936eb9577f3a165e
RockerFlower <kira@glorylab.xyz>
2024-07-25 18:10:33 +0900

    Enable Gyroscope

--- app/components/3d-card.tsx
+++ app/components/3d-card.tsx
@@ -1,152 +1,204 @@
 import React, {
-  createContext,
-  useState,
-  useContext,
-  useRef,
-  useEffect,
-} from "react";
-import { cn } from "~/src/cn";
-
-const MouseEnterContext = createContext<
-  [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
->(undefined);
-
-export const CardContainer = ({
-  children,
-  className,
-  containerClassName,
-}: {
-  children?: React.ReactNode;
-  className?: string;
-  containerClassName?: string;
-}) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [isMouseEntered, setIsMouseEntered] = useState(false);
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!containerRef.current) return;
-    const { left, top, width, height } =
-      containerRef.current.getBoundingClientRect();
-    const x = (e.clientX - left - width / 2) / 25;
-    const y = (e.clientY - top - height / 2) / 25;
-    containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
-  };
-
-  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
-    setIsMouseEntered(true);
-    if (!containerRef.current) return;
-  };
-
-  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!containerRef.current) return;
-    setIsMouseEntered(false);
-    containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  };
-  return (
-    <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
-      <div
-        className={cn(
-          "py-20 flex items-center justify-center",
-          containerClassName
-        )}
-        style={{
-          perspective: "1000px",
-        }}
-      >
+    createContext,
+    useState,
+    useContext,
+    useRef,
+    useEffect,
+  } from "react";
+  import { cn } from "~/src/cn";
+  
+  interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
+    requestPermission?: () => Promise<'granted' | 'denied'>;
+  }
+  
+  const MouseEnterContext = createContext<
+    [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
+  >(undefined);
+  
+  export const CardContainer = ({
+    children,
+    className,
+    containerClassName,
+  }: {
+    children?: React.ReactNode;
+    className?: string;
+    containerClassName?: string;
+  }) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [isMouseEntered, setIsMouseEntered] = useState(false);
+    const [isGyroEnabled, setIsGyroEnabled] = useState(false);
+  
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+      if (!containerRef.current || isGyroEnabled) return;
+      const { left, top, width, height } =
+        containerRef.current.getBoundingClientRect();
+      const x = (e.clientX - left - width / 2) / 25;
+      const y = (e.clientY - top - height / 2) / 25;
+      containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    };
+  
+    const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+      setIsMouseEntered(true);
+    };
+  
+    const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+      if (!containerRef.current) return;
+      setIsMouseEntered(false);
+      containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    };
+  
+    const handleOrientation = (event: DeviceOrientationEvent) => {
+      if (!containerRef.current || !isGyroEnabled) return;
+      const { beta, gamma } = event;
+      const isLandscape = window.matchMedia('(orientation: landscape)').matches;
+      if (beta !== null && gamma !== null) {
+        const x = isLandscape ? gamma : beta;
+        const y = isLandscape ? beta : gamma;
+        containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+      }
+    };
+  
+    useEffect(() => {
+      const initiate = async () => {
+        const requestPermission = (DeviceOrientationEvent as unknown as DeviceOrientationEventiOS).requestPermission;
+        const iOS = typeof requestPermission === 'function';
+        if (iOS) {
+          const permission = await requestPermission();
+          if (permission === 'granted') {
+            window.addEventListener("deviceorientation", handleOrientation);
+          }
+        } else {
+          window.addEventListener("deviceorientation", handleOrientation);
+        }
+      };
+  
+      if (isGyroEnabled) {
+        initiate();
+      } else {
+        window.removeEventListener('deviceorientation', handleOrientation);
+      }
+  
+      return () => {
+        window.removeEventListener('deviceorientation', handleOrientation);
+      };
+    }, [isGyroEnabled]);
+  
+    return (
+      <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
         <div
-          ref={containerRef}
-          onMouseEnter={handleMouseEnter}
-          onMouseMove={handleMouseMove}
-          onMouseLeave={handleMouseLeave}
           className={cn(
-            "flex items-center justify-center relative transition-all duration-200 ease-linear",
-            className
+            "py-20 flex flex-col items-center justify-center",
+            containerClassName
           )}
           style={{
-            transformStyle: "preserve-3d",
+            perspective: "1000px",
           }}
         >
-          {children}
+          <div
+            ref={containerRef}
+            onMouseEnter={handleMouseEnter}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            className={cn(
+              "flex items-center justify-center relative transition-all duration-200 ease-linear",
+              className
+            )}
+            style={{
+              transformStyle: "preserve-3d",
+            }}
+          >
+            {children}
+          </div>
+          <div className="mt-4">
+            <label className="flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                checked={isGyroEnabled}
+                onChange={() => setIsGyroEnabled(!isGyroEnabled)}
+                className="sr-only peer"
+              />
+              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Enable Gyroscope</span>
+            </label>
+          </div>
         </div>
-      </div>
-    </MouseEnterContext.Provider>
-  );
-};
-
-export const CardBody = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div
-      className={cn(
-        "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-};
-
-export const CardItem = ({
-  as: Tag = "div",
-  children,
-  className,
-  translateX = 0,
-  translateY = 0,
-  translateZ = 0,
-  rotateX = 0,
-  rotateY = 0,
-  rotateZ = 0,
-  ...rest
-}: {
-  as?: React.ElementType;
-  children: React.ReactNode;
-  className?: string;
-  translateX?: number | string;
-  translateY?: number | string;
-  translateZ?: number | string;
-  rotateX?: number | string;
-  rotateY?: number | string;
-  rotateZ?: number | string;
-  [key: string]: any;
-}) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [isMouseEntered] = useMouseEnter();
-
-  useEffect(() => {
-    handleAnimations();
-  }, [isMouseEntered]);
-
-  const handleAnimations = () => {
-    if (!ref.current) return;
-    if (isMouseEntered) {
-      ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
-    } else {
-      ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
-    }
+      </MouseEnterContext.Provider>
+    );
   };
-
-  return (
-    <Tag
-      ref={ref}
-      className={cn("w-fit transition duration-200 ease-linear", className)}
-      {...rest}
-    >
-      {children}
-    </Tag>
-  );
-};
-
-// Create a hook to use the context
-export const useMouseEnter = () => {
-  const context = useContext(MouseEnterContext);
-  if (context === undefined) {
-    throw new Error("useMouseEnter must be used within a MouseEnterProvider");
-  }
-  return context;
-};
+  
+  export const CardBody = ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className?: string;
+  }) => {
+    return (
+      <div
+        className={cn(
+          "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
+          className
+        )}
+      >
+        {children}
+      </div>
+    );
+  };
+  
+  export const CardItem = ({
+    as: Tag = "div",
+    children,
+    className,
+    translateX = 0,
+    translateY = 0,
+    translateZ = 0,
+    rotateX = 0,
+    rotateY = 0,
+    rotateZ = 0,
+    ...rest
+  }: {
+    as?: React.ElementType;
+    children: React.ReactNode;
+    className?: string;
+    translateX?: number | string;
+    translateY?: number | string;
+    translateZ?: number | string;
+    rotateX?: number | string;
+    rotateY?: number | string;
+    rotateZ?: number | string;
+    [key: string]: any;
+  }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const [isMouseEntered] = useMouseEnter();
+  
+    useEffect(() => {
+      handleAnimations();
+    }, [isMouseEntered]);
+  
+    const handleAnimations = () => {
+      if (!ref.current) return;
+      if (isMouseEntered) {
+        ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
+      } else {
+        ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+      }
+    };
+  
+    return (
+      <Tag
+        ref={ref}
+        className={cn("w-fit transition duration-200 ease-linear", className)}
+        {...rest}
+      >
+        {children}
+      </Tag>
+    );
+  };
+  
+  export const useMouseEnter = () => {
+    const context = useContext(MouseEnterContext);
+    if (context === undefined) {
+      throw new Error("useMouseEnter must be used within a MouseEnterProvider");
+    }
+    return context;
+  };
