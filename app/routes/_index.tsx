f4e5902b4775202715d1e6e93b93fd951d9e30b9
Kira <kira@glorylab.xyz>
2024-07-31 23:45:51 +0900

    Add sparkles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,7 @@ import footerPositionAtom from "~/atoms/footer-position-atom";
 import { Boxes } from "~/components/background-boxes";
 import { BackgroundGradientAnimation } from "~/components/background-gradient-animation";
 import AddressInputComponent from "~/components/poap/address-input";
+import { SparklesCore } from "~/components/sparkles";
 import { cn } from "~/src/cn";
 
 export const meta: MetaFunction = () => {
@@ -44,10 +45,30 @@ export default function Index() {
     <div className=" min-h-[2048px] w-full flex flex-col">
       <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-none [mask-image:linear-gradient(0deg,rgba(0,0,0,0),#000)]">
         <BackgroundGradientAnimation>
-          <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+          <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
             <p className="drop-shadow-2xl text-shadow">
               Dive into the world of POAP
             </p>
+            <div className="w-[40rem] h-40 relative mt-4">
+              {/* Gradients */}
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-white/90 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-white to-transparent h-[1px] w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-white/80 to-transparent h-[3px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-white to-transparent h-[1px] w-1/4" />
+
+              {/* Core component */}
+              <SparklesCore
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={500}
+                className="w-full h-full"
+                particleColor="#FFFFFF"
+              />
+
+              {/* Radial Gradient to prevent sharp edges */}
+              {/* <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div> */}
+            </div>
           </div>
         </BackgroundGradientAnimation>
       </div>
