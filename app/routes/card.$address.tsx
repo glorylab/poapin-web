4e44d1d3bdc57c6f157ccb108e9bca96cac9c95c
Kira <kira@glorylab.xyz>
2025-04-02 16:07:01 +0900

    Add og image for card/address

--- app/routes/card.$address.tsx
+++ app/routes/card.$address.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunctionArgs, json } from "@remix-run/cloudflare";
+import { LoaderFunctionArgs, json, MetaFunction } from "@remix-run/cloudflare";
 import { useLoaderData, useNavigate } from "@remix-run/react";
 import { useEffect } from "react";
 import { useAtom } from "jotai";
@@ -6,13 +6,85 @@ import { showResultsAtom, walletAddressAtom } from "~/atoms/address";
 import { AddressDisplay } from "~/components/card/address-display";
 import { ResultBox } from "~/components/card/result-box";
 import { motion } from "framer-motion";
+import { getFrameMetadata } from '@coinbase/onchainkit/frame';
+import { getEnv } from "~/src/env";
 
-export async function loader({ params }: LoaderFunctionArgs) {
+export const meta: MetaFunction = ({ data }) => {
+    const loaderData = data as LoaderData | undefined;
+
+    if (!loaderData || !loaderData.meta) {
+        return [];
+    }
+
+    const { title, description, address, ogimageurl } = loaderData.meta;
+
+    const baseMeta = [
+        { title },
+        { description },
+        { property: "og:title", content: title },
+        { property: "og:image", content: ogimageurl },
+        { property: "og:description", content: description },
+        { property: "og:site_name", content: "POAPin" },
+        { property: "og:type", content: "website" },
+        { property: "og:url", content: `https://poap.in/card/${address}` },
+    ];
+
+    const twitterMeta = [
+        { name: "X:card", content: "summary_large_image" },
+        { name: "X:domain", content: "poap.in" },
+        { name: "X:url", content: `https://poap.in/card/${address}` },
+        { name: "X:title", content: title },
+        { name: "X:description", content: description },
+        { name: "X:image", content: ogimageurl }
+    ];
+
+    const frameMetadata = getFrameMetadata({
+        buttons: [
+            {
+                action: 'link',
+                target: `https://poap.in/card`,
+                label: 'Get my POAP card',
+            },
+        ],
+        image: ogimageurl,
+    });
+    const frameMeta = Object.entries(frameMetadata).map(([key, value]) => ({ name: key, content: value }));
+
+    return [...baseMeta, ...twitterMeta, ...frameMeta];
+};
+
+interface LoaderData {
+    address: string;
+    meta?: {
+        title: string;
+        description: string;
+        address: string;
+        ogimageurl: string;
+    };
+}
+
+export async function loader({ params, context }: LoaderFunctionArgs) {
     const address = params.address;
     if (!address) {
         throw new Response("Not Found", { status: 404 });
     }
-    return json({ address });
+
+    // Create meta information
+    const metaTitle = `POAP Card for ${address} | POAPin`;
+    const metaDescription = `Visualize your POAP collection in a beautiful way. Create your own POAP card and share it with the world.`;
+    
+    // Get the OG image URL
+    const ogimageurl = `https://og.poap.in/api/poap/v/${address}/letter`;
+
+    return json({
+        address,
+        meta: {
+            title: metaTitle,
+            description: metaDescription,
+            address,
+            ogimageurl,
+        }
+    });
 }
 
 export default function CardAddressPage() {
