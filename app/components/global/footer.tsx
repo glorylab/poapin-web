263c17e7a18f2654c57a053512590361a2829afe
Kira <kira@glorylab.xyz>
2025-04-10 00:25:24 +0900

    Powered by POAP

--- app/components/global/footer.tsx
+++ app/components/global/footer.tsx
@@ -45,6 +45,17 @@ export default function FooterComponent() {
     return (
         <div className="flex w-full flex-col border-t-1 border-neutral-300 z-0">
             <div className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-6 py-12 lg:px-8">
+
+                <Link to="https://poap.xyz" target="_blank">
+                    <div className="flex items-center justify-center rounded-md bg-white border-1 border-[#5C5AA0] px-4 py-2 shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] transition-all hover:scale-[1.02] active:scale-[0.98]">
+                        <img src="/images/logo_poap.svg" alt="POAP Logo" className="h-8 w-8 mr-2" />
+                        <div className="flex flex-col">
+                            <span className="text-xs text-[#5C5AA0]">Powered by</span>
+                            <span className="text-xl font-bold text-[#5C5AA0]">POAP</span>
+                        </div>
+                    </div>
+                </Link>
+                <Spacer y={2} />
                 <div className="flex items-center justify-center">
                     <span className="text-medium text-default font-medium">POAP.in</span>
                 </div>
