6979bc2d0afc559c6de8e53bfd82cf72165613ed
Kira <kira@glorylab.xyz>
2025-06-26 15:41:16 +0900

    Inject Plausible script

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,34 +1,52 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
 import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToReadableStream } from "react-dom/server";
+
+function Document({
+  children,
+  isProd,
+}: {
+  children: React.ReactNode;
+  isProd: boolean;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        {isProd && (
+          <script
+            defer
+            data-domain="poap.in"
+            src="https://insights.glorylab.xyz/js/script.js"
+          />
+        )}
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  loadContext: AppLoadContext,
+  loadContext: AppLoadContext
 ) {
+  const env = loadContext.cloudflare.env as Record<string, string>;
+  const isProd = env.ENVIRONMENT === "production";
+
   const body = await renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
+    <Document isProd={isProd}>
+      <RemixServer context={remixContext} url={request.url} />
+    </Document>,
     {
       signal: request.signal,
-      onError(error: unknown) {
-        // Log streaming rendering errors from inside the shell
+      onError(error) {
         console.error(error);
         responseStatusCode = 500;
       },
-    },
+    }
   );
 
   if (isbot(request.headers.get("user-agent") || "")) {
